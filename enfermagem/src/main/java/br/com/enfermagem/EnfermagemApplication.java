b9ec672ed7fab70dec7c61329f1a7c1ab4a146a9
unknown <jdutrarodrigues@gmail.com>
2019-10-27 15:29:53 -0300

    Estrutura inicial API

---
+++ enfermagem/src/main/java/br/com/enfermagem/EnfermagemApplication.java
@@ -0,0 +1,13 @@
+package br.com.enfermagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnfermagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnfermagemApplication.class, args);
+	}
+
+}
---
+++ enfermagem/src/test/java/br/com/enfermagem/EnfermagemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.enfermagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnfermagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
